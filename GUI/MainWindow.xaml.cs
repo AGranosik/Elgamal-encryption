fb3b611727e1bf0660d99360ece21226b96f78f8
AGranosik <adrian.granosik@gmail.com>
2018-10-17 01:08:13 +0200

    GUI - Load from file works fine

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Elgamal_cryptography.Display.Interfaces;
 using Elgamal_cryptography.Encriptions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,29 +49,48 @@ namespace GUI
             _elgamal.GeneratePublicKeys();
             _elgamal.GeneratePrivateKeys(MessageHandler.GenerateMessage());
             PText.Content = _elgamal.P.ToString();
-            KText.Content = _elgamal.K.ToString();
+            BText.Content = _elgamal.B.ToString();
+            GText.Content = _elgamal.G.ToString();
             RText.Content = _elgamal.R.ToString();
             SText.Content = _elgamal.Ss.ToString();
+            MessageField.Content = _elgamal.M.ToString();
         }
 
-        public void LoadFromFile()
+        public void LoadFromFile(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
 
-        public void MainMenu()
-        {
-            throw new NotImplementedException();
-        }
+            openFileDialog.Multiselect = false;
 
-        public void SaveToFile()
-        {
-            throw new NotImplementedException();
-        }
+            //if you want filter all files            
+            openFileDialog.DefaultExt = ".txt"; // Default file extension 
+            openFileDialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+            openFileDialog.ShowDialog();
 
+            try
+            {
+                using(StreamReader sr = new StreamReader(openFileDialog.FileName))
+                {
+                    _elgamal.P = int.Parse(sr.ReadLine());
+                    _elgamal.A = int.Parse(sr.ReadLine());
+                    _elgamal.B = UInt64.Parse(sr.ReadLine());
+                    _elgamal.G = int.Parse(sr.ReadLine());
+                    _elgamal.R = UInt64.Parse(sr.ReadLine());
+                    _elgamal.Ss = UInt64.Parse(sr.ReadLine());
+                    _elgamal.M = int.Parse(sr.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            PText.Content = _elgamal.P.ToString();
+            BText.Content = _elgamal.B.ToString();
+            GText.Content = _elgamal.G.ToString();
+            RText.Content = _elgamal.R.ToString();
+            SText.Content = _elgamal.Ss.ToString();
+            MessageField.Content = _elgamal.M.ToString();
         }
     }
 }
